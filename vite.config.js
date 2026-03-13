94bbfc2208c9de927ebcb1b60af0520969f63b84
Jasonmh <jasonnmh@gmail.com>
2026-03-13 22:26:14 +0200

    9 commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ['@vercel/analytics'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 })
